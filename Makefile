651e92a3e8a051f853b6afd1bce66bd1a86b1e00
Janusz Grabis <janusz.grabis-ext@hexagon.com>
2025-06-02 15:20:51 +0200

    Configuration that build cucumber-cpp/0.7.0 with conan 1.60.0

---
+++ Makefile
@@ -0,0 +1,2 @@
+conan_create:
+	conan create . -pr hxdm/x86_64-windows-vs2022-debug
---
+++ conanfile.py
@@ -0,0 +1,51 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+
+
+class CucumberCppConan(ConanFile):
+    name = "cucumber-cpp"
+    version = "0.7.0"
+
+    # Optional metadata
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of CucumberCpp here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "include/*", "cmake/*"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def requirements(self):
+        self.requires("nlohmann_json/3.10.5")
+        self.requires("asio/1.27.0")
+        self.requires("tclap/1.2.5") #not available in our repo, needs conan-center
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["cucumber-cpp"]
